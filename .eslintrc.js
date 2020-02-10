a00c4b54ba0301def6197390f64fff81ad367262
xaviguasch <xaviguasch@gmail.com>
2020-02-10 11:40:04 +0100

    added validation logic

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   parser: 'babel-eslint',
 
   rules: {
-    indent: ['error', 'tab'],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never']
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log('connected successfully')
+const form = document.getElementById('main-form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+// Show input error message
+const showError = (input, message) => {
+  const formGroup = input.parentElement
+
+  formGroup.className = 'form-group error'
+
+  const small = formGroup.querySelector('small')
+
+  small.innerText = message
+}
+
+// Show success outline
+const showSuccess = input => {
+  const formGroup = input.parentElement
+
+  formGroup.className = 'form-group success'
+}
+
+// Check if email is valid
+const isValidEmail = email => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  return re.test(String(email).toLowerCase())
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  if (username.value === '') {
+    showError(username, 'Username is required')
+  } else {
+    showSuccess(username)
+  }
+
+  if (email.value === '') {
+    showError(email, 'email is required')
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'email is not valid')
+  } else {
+    showSuccess(email)
+  }
+
+  if (password.value === '') {
+    showError(password, 'password is required')
+  } else {
+    showSuccess(password)
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'password2 is required')
+  } else {
+    showSuccess(password2)
+  }
+})
