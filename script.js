63d0e267b5b11646cedb373d7dbb0f0c0985e8b2
xaviguasch <xaviguasch@gmail.com>
2020-02-10 11:59:54 +0100

    refactored with checkRequired function

--- script.js
+++ script.js
@@ -24,38 +24,57 @@ const showSuccess = input => {
 
 // Check if email is valid
 const isValidEmail = email => {
+  // eslint-disable-next-line no-useless-escape
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   return re.test(String(email).toLowerCase())
+}
+
+// Get fieldname
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+// Check required fields
+const checkRequired = inputArr => {
+  inputArr.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSuccess(input)
+    }
+  })
 }
 
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault()
 
-  if (username.value === '') {
-    showError(username, 'Username is required')
-  } else {
-    showSuccess(username)
-  }
+  checkRequired([username, email, password, password2])
 
-  if (email.value === '') {
-    showError(email, 'email is required')
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'email is not valid')
-  } else {
-    showSuccess(email)
-  }
+  // if (username.value === '') {
+  //   showError(username, 'Username is required')
+  // } else {
+  //   showSuccess(username)
+  // }
 
-  if (password.value === '') {
-    showError(password, 'password is required')
-  } else {
-    showSuccess(password)
-  }
+  // if (email.value === '') {
+  //   showError(email, 'email is required')
+  // } else if (!isValidEmail(email.value)) {
+  //   showError(email, 'email is not valid')
+  // } else {
+  //   showSuccess(email)
+  // }
 
-  if (password2.value === '') {
-    showError(password2, 'password2 is required')
-  } else {
-    showSuccess(password2)
-  }
+  // if (password.value === '') {
+  //   showError(password, 'password is required')
+  // } else {
+  //   showSuccess(password)
+  // }
+
+  // if (password2.value === '') {
+  //   showError(password2, 'password2 is required')
+  // } else {
+  //   showSuccess(password2)
+  // }
 })
